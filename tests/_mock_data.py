7c38eaaf638319bbd029efe3dca98184a816596c
Jill Cates <cates.jill@gmail.com>
2018-09-02 12:38:43 -0700

    Remove redundant helper functions from tests, refactor

--- tests/_mock_data.py
+++ tests/_mock_data.py
@@ -33,44 +33,19 @@ def create_ratings_df():
 # Transformer() input
 # ---------------------------------------------------------------------
 
-def gen_fetched_data():
+def create_user_item_dict():
     """
     Generates inputs of Transformer
-    for basic unit testing
+    for basic unit testing.
     """
+    user_item_dict = dict()
     distinct_users = np.array([111, 222, 333, 444, 555, 666, 777, 888])
     distinct_items = np.array([201, 202, 203, 304, 305, 306])
-    fetched_cs = np.array([(111, 201, 1), (333, 203, 1), (777, 306, 1)])
-    correct_dense = np.zeros((len(distinct_users), len(distinct_items)))
-    correct_dense[0, 0] = 1
-    correct_dense[2, 2] = 1
-    correct_dense[6, 5] = 1
-    uc_dict = {
-        'item_user_score': fetched_cs,
-        'user_id': distinct_users,
-        'item_id': distinct_items
-    }
-    return uc_dict, correct_dense
-
-
-def gen_bad_user_data():
-    """Helper to generate bad user id data"""
-    bad_user_dict = {
-        'item_user_score': np.array([(12, 12, 1), (13, 14, 0)]),
-        'user_id': np.array([1, 12]),
-        'item_id': np.array([12, 14])
-    }
-    return bad_user_dict
-
-
-def gen_bad_coll_data():
-    """Helper to generate bad item id data"""
-    bad_user_dict = {
-        'item_user_score': np.array([(12, 12, 1), (13, 14, 0)]),
-        'user_id': np.array([12, 13]),
-        'item_id': np.array([1, 14])
-    }
-    return bad_user_dict
+    user_item_score = np.array([(111, 201, 1), (333, 203, 1), (777, 306, 1)])
+    user_item_dict['distinct_users'] = distinct_users
+    user_item_dict['distinct_items'] = distinct_items
+    user_item_dict['user_item_score'] = user_item_score
+    return user_item_dict
 
 
 # ---------------------------------------------------------------------
@@ -90,7 +65,6 @@ def sparse_array():
 # ---------------------------------------------------------------------
 # Recommendations dictionary
 # ---------------------------------------------------------------------
-
 
 def recommendations_dict():
     """
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -9,7 +9,7 @@ import pytest
 import numpy as np
 
 from implicitmf.transform import Transformer
-from _mock_data import gen_fetched_data, gen_bad_user_data, gen_bad_coll_data
+from _mock_data import create_user_item_dict
 
 def test_transformer_input_dict_error():
     """
@@ -25,18 +25,18 @@ def test_transformer_input_bool_error():
     Check that Transformer raises a TypeError
     if full_matrix is not a boolean.
     """
-    uc_dict, _ = gen_fetched_data()
+    ui_dict = create_user_item_dict()
     msg = "`full_matrix` must be a boolean"
     with pytest.raises(TypeError, match=msg):
-        Transformer(user_item_dict=uc_dict, full_matrix="bool")
+        Transformer(user_item_dict=ui_dict, full_matrix="bool")
 
 def test_transformer_attribute_type():
     """
     Check that Transformer mapper attrs are
     the correct type.
     """
-    uc_dict, _ = gen_fetched_data()
-    t = Transformer(uc_dict)
+    ui_dict = create_user_item_dict()
+    t = Transformer(ui_dict)
     assert isinstance(t.item_inv_mapper, dict)
     assert isinstance(t.user_mapper, dict)
 
@@ -45,27 +45,17 @@ def test_transformer_shape():
     Check that Transformer.to_sparse_array
     returns to correct shape.
     """
-    uc_dict, _ = gen_fetched_data()
-    t = Transformer(uc_dict)
+    ui_dict = create_user_item_dict()
+    t = Transformer(ui_dict)
     X = t.to_sparse_array()
-    assert X.shape == (len(uc_dict['user_id']), len(uc_dict['item_id']))
+    assert X.shape == (len(ui_dict['user_id']), len(ui_dict['item_id']))
 
 def test_transformer_num_nonzero():
     """
     Check that Transformer.to_sparse_array's
     non-zero elements are the correct length.
     """
-    uc_dict, _ = gen_fetched_data()
-    t = Transformer(uc_dict)
+    ui_dict = create_user_item_dict()
+    t = Transformer(ui_dict)
     X = t.to_sparse_array()
-    assert X.getnnz() == len(uc_dict['item_user_score'])
-
-def test_transformer_loc_nonzero():
-    """
-    Check that Transofrmer.to_sparse_array's
-    non-zero elements are in the correct location.
-    """
-    uc_dict, correct = gen_fetched_data()
-    t = Transformer(uc_dict)
-    X = t.to_sparse_array()
-    assert np.array_equal(X.toarray(), correct)
+    assert X.getnnz() == len(ui_dict['item_user_score'])
